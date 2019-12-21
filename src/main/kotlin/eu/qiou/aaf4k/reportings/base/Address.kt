47bbb0493678f47261083c29c249fd8e90f6945b
6234456 <6234456@sina.com>
2019-12-21 22:08:49 +0100

    Modify Base Class

--- src/main/kotlin/eu/qiou/aaf4k/reportings/base/Address.kt
+++ src/main/kotlin/eu/qiou/aaf4k/reportings/base/Address.kt
@@ -1,12 +1,12 @@
 package eu.qiou.aaf4k.reportings.base
 
+import eu.qiou.aaf4k.reportings.GlobalConfiguration
 import eu.qiou.aaf4k.util.io.JSONable
 import java.util.*
 
-data class Address(val id: Int, var country: Locale, var province: String, var city: String, var zipCode: String, var street: String, var number: String) :
+data class Address(val id: Int, var country: Locale = GlobalConfiguration.DEFAULT_LOCALE, var province: String = "", var city: String = "", var zipCode: String = "", var street: String = "", var number: String = "") :
     JSONable {
     override fun toJSON(): String {
         return """{"id":$id, "country":"${country.country}", "province":"$province", "city":"$city", "zipCode":"$zipCode", "street":"$street", "number":"$number"}"""
     }
-
 }
