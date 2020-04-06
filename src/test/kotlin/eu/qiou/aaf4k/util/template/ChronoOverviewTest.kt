877284b00740b1e664670d1ce653c24ced128dbb
6234456 <6234456@sina.com>
2020-04-06 21:31:11 +0200

    add test case template

---
+++ src/test/kotlin/eu/qiou/aaf4k/util/template/ChronoOverviewTest.kt
@@ -0,0 +1,43 @@
+package eu.qiou.aaf4k.util.template
+
+import eu.qiou.aaf4k.util.time.TimeSpan
+import org.junit.Assert.*
+import org.junit.Test
+
+class ChronoOverviewTest {
+    @Test
+    fun trail() {
+        WorkingPaper.gloablProcessor = "WinH"
+        WorkingPaper.globalTheme = Template.Theme.LAVANDA
+
+        hierarchy("trail") {
+            document("Prüfung Rückstellungen", DevelopmentOfAccount::class) {
+                data = listOf(
+                    mapOf("Name" to "Demo1", "Anfangsbestand" to 100, "Zugang" to 20, "Abgang" to 9, "Umbuchung" to 0),
+                    mapOf("Name" to "Demo1", "Anfangsbestand" to 100, "Abgang" to 20, "Zugang" to 9, "Umbuchung" to 0),
+                    mapOf("Name" to "Demo1", "Anfangsbestand" to 100, "Zugang" to 20, "Abgang" to 9, "Umbuchung" to 0)
+                )
+                processedBy = "WindHunter"
+                entityName = "Trail AG"
+            }
+            hierarchy("sub2") {
+                document("Review ARAP", SimpleOverview::class) {
+                    data = listOf(
+                        mapOf("Name" to "Demo1", "Summe" to 32320),
+                        mapOf("Name" to "Demo1", "Summe" to 32320), mapOf("Name" to "Demo1", "Summe" to 32320),
+                        mapOf("Name" to "Demo1", "Summe" to 32320), mapOf("Name" to "Demo1", "Summe" to 32320),
+                        mapOf("Name" to "Demo1", "Summe" to 32320), mapOf("Name" to "Demo1", "Summe" to 32320),
+                        mapOf("Name" to "Demo1", "Summe" to 32320),
+                        mapOf("Name" to "Demo1", "Summe" to 32320)
+                    )
+                    entityName = "Demo AG"
+                }
+                hierarchy("sub3") {
+                    hierarchy("sub4") {
+
+                    }
+                }
+            }
+        }.generate(root = "/home/yang/Documents")
+    }
+}
